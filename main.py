29f4e5f56b610271accf0cb82f88b1605342f2db
William Riegel <WRiegel19021@rpc.net>
2020-11-19 06:05:59 -0600

    main 1

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
 
     my_output = ''
     if showGitHubVars == "true":
+        my_output = 'Git Hub Vars'
         CIEnv = os.environ["CI"]
         GITHUB_WORKFLOWEnv = os.environ["GITHUB_WORKFLOW"]
         GITHUB_RUN_IDEnv = os.environ["GITHUB_RUN_ID"]
@@ -19,14 +20,19 @@ def main():
          
         # newline = %0A
 
-        my_output = f"CI {CIEnv} GITHUB_WORKFLOW {GITHUB_WORKFLOWEnv} GITHUB_RUN_ID {GITHUB_RUN_IDEnv}" +  newLineEsc \
-                    + f"GITHUB_RUN_NUMBER {GITHUB_RUN_NUMBEREnv}" 
+        my_output += newLineEsc + f"CI {CIEnv}" + newLineEsc  \
+            + f"GITHUB_WORKFLOW {GITHUB_WORKFLOWEnv}" + newLineEsc  \
+            + f"GITHUB_RUN_ID {GITHUB_RUN_IDEnv}" + newLineEsc \
+            + f"GITHUB_RUN_NUMBER {GITHUB_RUN_NUMBEREnv}" 
+
+
 
     if len(listOfAppEnvVars) > 0:
+        my_output += "App Env Vars " + newLineEsc
         my_list = listOfAppEnvVars.split(",")
-        # for env in my_list:
-
-
+        for appEnvVar in my_list:
+            appEnvValue = os.environ[appEnvVar]
+            my_output +=  f"{appEnvVar} {appEnvValue}" + newLineEsc
 
 
     print(f"::set-output name=myOutput::{my_output}")
