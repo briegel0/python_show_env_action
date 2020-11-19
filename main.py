c371b6baf2c38c97e05a888c7b451084089413d0
William Riegel <WRiegel19021@rpc.net>
2020-11-19 05:31:53 -0600

    line 7

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
         GITHUB_WORKFLOWEnv = os.environ["GITHUB_WORKFLOW"]
         GITHUB_RUN_IDEnv = os.environ["GITHUB_RUN_ID"]
         GITHUB_RUN_NUMBEREnv = os.environ["GITHUB_RUN_NUMBER"]
+         
+        # newline = %0A
+
         my_output = (f"CI {CIEnv} GITHUB_WORKFLOW {GITHUB_WORKFLOWEnv} GITHUB_RUN_ID {GITHUB_RUN_IDEnv}%0A" 
                     + f"GITHUB_RUN_NUMBER {GITHUB_RUN_NUMBEREnv}" )
 
